7129fd372412aa1f3677b28f65c1c21b92834f7a
walidhkhan <walidhkhan@gmail.com>
2021-08-16 18:46:52 -0700

    added animal creation

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -30,20 +38,47 @@ function filterByQuery(query, animalsArray) {
     }
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    } 
+    }
     if (query.species) {
         filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    } 
+    }
     if (query.name) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    } 
+    }
     // return the filtered results;
     return filteredResults;
-} 
+}
 
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
+}
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
 }
 
 // adds route 
@@ -58,12 +93,25 @@ app.get('/api/animals', (req, res) => {
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
-      res.json(result);
+        res.json(result);
     } else {
         res.send(404);
     }
 });
 
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+});
 
 // using listen() in order to make the the server respond to requests
 app.listen(PORT, () => {
